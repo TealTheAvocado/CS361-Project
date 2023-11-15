bb1a7e02e83105d602ae013f8b862b288ee12bf6
Flora Zhang <zhangfl@oregonstate.edu>
2023-11-14 20:55:28 -0800

    Update to 6 flashcards

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from urllib.parse import quote_plus
+import requests
 
 app = Flask(__name__, template_folder='pages')
 
@@ -10,7 +11,16 @@ client = MongoClient(MONGO_URI, server_api = ServerApi('1'))
 db = client.flashcards
 collection = db.flashcards
 
+def get_data_from_microservice():
+    microservice_url = 'http://127.0.0.1:5002/get_data'
+    response = requests.get(microservice_url)
 
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return "Failed to fetch data from the microservice."
+    
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -22,31 +32,8 @@ def about():
 @app.route('/flashcards')
 def display_flashcards():
     # Display blank flashcards for now
-    flashcards = [
-                {
-            'shanghainese': 'Shanghainese',
-            'phonetics': 'Phonetics',
-            'english': 'English'
-        },
-        {
-            'shanghainese': 'Shanghainese',
-            'phonetics': 'Phonetics',
-            'english': 'English'
-        },
-        {
-            'shanghainese': 'Shanghainese',
-            'phonetics': 'Phonetics',
-            'english': 'English'
-        },
-        {
-            'shanghainese': 'Shanghainese',
-            'phonetics': 'Phonetics',
-            'english': 'English'
-        }
-    ]  
-
+    flashcards = get_data_from_microservice()
     return render_template('flashcards.html', flashcards=flashcards)
-
 
 
 @app.route('/database')
